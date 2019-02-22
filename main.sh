77b97beba12fb7df08f445cc6eefbd9a6b3a9e8f
china-digital-times <cdt@chinadigitaltimes.net>
2019-02-22 17:37:52 -0500

    init

--- main.sh
+++ main.sh
@@ -5,7 +5,7 @@ offset=1714
 max=174100
 
 while [ $offset -le $max ]; do
-    curl -s -o "data/$n.json" "https://chinadigitaltimes.net/chinese/wp-json/wp/v2/posts/?order=asc&per_page=100&orderby=id&offset=${offset}"
+    curl -s -o "list/$n.json" "https://chinadigitaltimes.net/chinese/wp-json/wp/v2/posts/?order=asc&per_page=100&orderby=id&offset=${offset}"
     echo "$n.json"
     offset=$(($offset+100))
     n=$(($n+1))
